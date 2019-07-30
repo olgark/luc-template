4d1247415d551b8cd4e51957d8e47e3b044eb669
sravenscraft <sravenscraft@luc.edu>
2019-07-30 17:07:22 -0500

    Working on accessible menu upate

--- deploy/js/all.js
+++ deploy/js/all.js
@@ -225,3 +225,33 @@ $.fn.setup_navigation = function(settings) {
 		e.stopPropagation();
 	});
 }
+
+function openSearch() {
+  document.getElementById("search-overlay").style.display = "block";
+}
+
+function closeSearch() {
+  document.getElementById("search-overlay").style.display = "none";
+}
+
+$(document).keyup(function(e) {
+  if (e.which == 27) {
+    $(".overlay").fadeOut(300);
+  }
+});
+
+//slide out
+
+$("#information-button").click(function(){
+  $("#information-menu").animate({
+    right: '0px',
+    opacity: '1'
+  });
+});
+
+$("#information-button").mouseout(function(){
+  $("#information-menu").animate({
+    right: '-250px',
+    opacity: '1'
+  });
+});
