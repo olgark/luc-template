046a834e58962b28f4db1f947f95c5bf74cd38b5
sravenscraft <sravenscraft@luc.edu>
2019-09-03 16:40:04 -0500

    Updated and added the js for the sliding menu

--- deploy/js/all.js
+++ deploy/js/all.js
@@ -966,6 +966,20 @@ function closeNav() {
   document.getElementById("SideNav").style.width = "0px";
 }
 
+// Get the input field
+var input = document.getElementById("SideNavOpen");
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("SideNavOpen").click();
+  }
+});
+
 var sideNavEscape = "SideNav";
 window.onkeyup = function (event) {
 if (event.keyCode == 27) {
