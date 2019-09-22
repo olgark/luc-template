907064cdc662b21423a07498ad4b22c8d96c8458
Stephen Ravenscraft <sravenscraft@luc.edu>
2019-09-22 15:49:30 -0500

    deleted escape functionality on side nav and search

--- development/js/search.js
+++ development/js/search.js
@@ -5,11 +5,3 @@ function openSearch() {
 function closeSearch() {
   document.getElementById("search-overlay").style.display = "none";
 }
-
-$(document).keyup(function(e) {
-  if (e.which == 27) {
-    $(".overlay").fadeOut(300);
-  }
-});
-
-//slide out
--- development/js/sliding-menu.js
+++ development/js/sliding-menu.js
@@ -1,16 +1,4 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-//function openNav() {
-//  document.getElementById("mySidenav").style.width = "250px";
-//  document.getElementById("main-wrapper").style.marginLeft = "250px";
-//}
-
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-//function closeNav() {
-//  document.getElementById("mySidenav").style.width = "0";
-//  document.getElementById("main-wrapper").style.marginLeft = "0";
-//}
-
-/* Set the width of the side navigation to 250px */
+/* Set the width of the side navigation to 300px */
 function openNav() {
   document.getElementById("SideNav").style.width = "300px";
 }
@@ -18,25 +6,4 @@ function openNav() {
 /* Set the width of the side navigation to 0 */
 function closeNav() {
   document.getElementById("SideNav").style.width = "0px";
-}
-
-// Get the input field
-var input = document.getElementById("SideNavOpen");
-
-// Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function(event) {
-  // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Trigger the button element with a click
-    document.getElementById("SideNavOpen").click();
-  }
-});
-
-var sideNavEscape = "SideNav";
-window.onkeyup = function (event) {
-if (event.keyCode == 27) {
-document.getElementById(sideNavEscape).style.width = "0px";
-}
 }
