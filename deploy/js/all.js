4c33dd61c36415a8b0e4a9d1cb8121abf47de22c
Stephen Ravenscraft <sravenscraft@luc.edu>
2019-09-23 16:37:12 -0500

    Cleaned up the JS directory

--- deploy/js/all.js
+++ deploy/js/all.js
@@ -945,9 +945,20 @@ function closeSearch() {
 /* Set the width of the side navigation to 300px */
 function openNav() {
   document.getElementById("SideNav").style.width = "300px";
+  $(SideNav).keyup(function(event){
+  	if (event.keyCode == 27){
+  		// Close the modal/menu
+  		$(modal_or_menu_element).closeElement();
+
+          	//  Return focus to the element that invoked it
+  		$('a[href=#SideNavOpen]').focus();
+  	}
+  });
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
   document.getElementById("SideNav").style.width = "0px";
 }
+
+// This code would be placed in the method that opens the modal/menu:
